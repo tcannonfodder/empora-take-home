b5b8e6105a494b38632f4d032960bb5279c594a1
Thomas Cannon <tcannon00@gmail.com>
2023-04-01 11:41:50 -0400

    WIP: Fill out test suite skeleton, add "need to implement" test stub
    * Gets the infrastructure in place but makes sure the tests are failing
    	so they are known to be missing

---
+++ test/address_verification_client_test.rb
@@ -0,0 +1,5 @@
+require "test_helper"
+
+class AddressVerificationClientTest < Minitest::Test
+  test "need to implement"
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+Bundler.require(:test)
+SimpleCov.start do
+  add_filter "/test/"
+end
+
+
+require 'webmock/minitest'
+WebMock.enable!
+
+require 'mocha/minitest'
+
+require_relative 'test_helper/declarative_test_patch'
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+
+require "minitest/autorun"

1bd2f37ee8b548098df626e8e51e5ba898808894
Thomas Cannon <tcannon00@gmail.com>
2023-04-01 11:54:26 -0400

    WIP: Add `validate_addresses.rb`
    * This is the actual ruby executable used to take a CSV and validate it
    * It uses `ARGF.to_io` to take the file contents (either STDIN or the
    	contents of the filenames provided) to build the input stream for
    	`AddressCSVTransformer`
    	* Also fetches the `SMARTY_AUTH_ID` and `SMARTY_AUTH_TOKEN` values
    		* This will raise an error if they have not been defined
    	* Prints the validates results

---
+++ validate_addresses.rb
@@ -0,0 +1,16 @@
+require 'bundler/setup'
+Bundler.require(:default)
+require_relative "lib/address_csv_transformer"
+require_relative "lib/address_verification_client"
+
+transformer = AddressCSVTransformer.new(
+  input_stream: ARGF.to_io,
+  smarty_auth_id: ENV.fetch("SMARTY_AUTH_ID"),
+  smarty_auth_token: ENV.fetch("SMARTY_AUTH_TOKEN")
+)
+
+transformer.validate!
+
+transformer.validated_results.values.each do |result|
+  puts result
+end
